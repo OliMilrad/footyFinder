3950f3825324e2c66076db4ac42a3d49eeea6ac2
Guido Lagar <guidolagar@gmail.com>
2023-03-13 17:47:50 -0300

    players controller done

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,22 +1,40 @@
 class PlayersController < ApplicationController
+  before_action :set_player, only: %i[show edit update destroy]
   def index
+    @players = Player.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @player = Player.new
   end
 
   def create
+    @player = Player.new(player_params)
+    @player.save
+    redirect_to players_path(@player)
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @player.update(player_params)
+    redirect_to players_path(@player)
   end
 
   def destroy
+    @player.destroy
+    redirect_to players_path, status: :see_other
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:date, :confirmed)
+  end
+
+  def set_player
+    @player = Player.find(params[:id])
   end
 end
