59cb98c460c917d5780d3e9bc883da72fa858f7c
Guido Lagar <guidolagar@gmail.com>
2023-03-13 15:14:38 -0300

    player model created

---
+++ db/migrate/20230313173050_create_players.rb
@@ -0,0 +1,16 @@
+class CreatePlayers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :players do |t|
+      t.string :full_name
+      t.string :position
+      t.string :nationality
+      t.string :location
+      t.float :height
+      t.integer :age
+      t.string :last_club
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
