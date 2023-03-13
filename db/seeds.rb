47bf13ed943e0359706fcd6d85bb795457b11cc9
Guido Lagar <guidolagar@gmail.com>
2023-03-13 17:27:26 -0300

    validations done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+100.times do
+  Post.create(title: Faker::Music.band, url: Faker::Sports::Football.player, votes: 2)
+end
+
+
+Player.create(full_name: , )
