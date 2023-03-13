4e9775dad6e2771c82538a7de78d4e900a9193d3
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-03-13 15:44:58 -0300

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'models/show'
-  get 'models/index'
-  get 'models/new'
-  get 'models/create'
-  get 'models/edit'
-  get 'models/update'
-  get 'models/destroy'
-  devise_for :users
   root to: "pages#home"
+  resources :bookings
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
