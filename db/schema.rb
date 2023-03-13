78ad37d858135a22646f37309515891b367aacc6
Guido Lagar <guidolagar@gmail.com>
2023-03-13 15:41:58 -0300

    player model done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_13_175015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +21,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_175015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "full_name"
+    t.string "position"
+    t.string "nationality"
+    t.string "location"
+    t.float "height"
+    t.integer "age"
+    t.string "last_club"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
